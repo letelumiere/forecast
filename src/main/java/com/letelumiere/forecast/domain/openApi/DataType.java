45e0e091089417178bfcc968ad8953bfb7a8f33b
letelumiere <letelumiere@gmail.com>
2023-12-22 19:56:09 +0900

    commits

--- src/main/java/com/letelumiere/forecast/domain/openApi/DataType.java
+++ src/main/java/com/letelumiere/forecast/domain/openApi/DataType.java
@@ -1,6 +1,15 @@
 package com.letelumiere.forecast.domain.openApi;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DataType {
-    XML, JSON;
+    XML("XML", "XML"),
+    JSON("JSON", "JSON");
+
+    private final String code;
+    private final String description;
 }
